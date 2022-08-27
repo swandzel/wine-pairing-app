dde0a2f350df0c3bd47cd030e977521d0d356e3c
swandzel <s.wandzel@o2.pl>
2022-08-27 17:21:55 +0200

    transition delay on logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./Logo.css";
+import { motion } from "framer-motion";
+import { fadeAnimation } from "../../utils/framer-animations";
 
 const Logo = ({ refreshPage }) => {
+  const { hidden, visible, fadeTransition } = fadeAnimation;
+
   return (
-    <div className="logo" onClick={refreshPage}>
+    <motion.div
+      initial={hidden}
+      animate={visible}
+      transition={fadeTransition}
+      className="logo"
+      onClick={refreshPage}
+    >
       <i className="fas fa-wine-glass-alt glass"></i>
       wine pairing app
-    </div>
+    </motion.div>
   );
 };
 
