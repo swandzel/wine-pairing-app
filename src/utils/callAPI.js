710b9d9f4ee02311de7b265f98a94e1eef9c1c0c
swandzel <s.wandzel@o2.pl>
2023-01-03 11:30:07 +0100

    API link change

--- src/utils/callAPI.js
+++ src/utils/callAPI.js
@@ -13,7 +13,7 @@ const callAPI = async (
     setLoader(true);
     try {
       const { data } = await axios.get(
-        `https://wine-pairing-app-api.herokuapp.com/getWine?food=${query.toLocaleLowerCase()}`
+        `https://wine-pairing-api.onrender.com/getWine?food=${query.toLocaleLowerCase()}`
       );
 
       if (data.status === "success") {
